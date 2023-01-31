ba847b76f6b34a404213e45b43cf0c55a7302081
Haoran SUN <shuenhoy@zju.edu.cn>
2023-01-31 09:27:15 +0800

    load worker into blob url

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,10 @@
 import typescript from 'rollup-plugin-typescript2';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import url from '@rollup/plugin-url';
 
 export default {
-  input: 'main.ts',
+  input: './src/main.ts',
   output: {
     dir: '.',
     sourcemap: 'inline',
@@ -14,7 +15,18 @@ export default {
   plugins: [
     nodeResolve({ browser: true }),
     typescript(),
+    {
+      name: "pdf.worker.min.js",
 
+      transform(code, id) {
+        if (id.endsWith('pdf.worker.min.js')) {
+          return {
+            code: `const code = ${JSON.stringify(code)};const blob = new Blob([code], { type: 'application/javascript' }); export default URL.createObjectURL(blob);`,
+            map: { mappings: "" }
+          };
+        }
+      }
+    },
     commonjs(),
   ]
 };
